97602576a3d7b42237c7f71db0a589abd4f16ed0
Shola Ayeni <ayenisholah@yahoo.com>
2024-07-04 07:56:40 +0100

    updated solana documentation

---
+++ src/examples/solana.ts
@@ -0,0 +1,21 @@
+// https://api-mainnet.magiceden.io/v2/instructions/mmm/create-pool?spotPrice=0.00525&solDeposit=0.00525&curveType=exp&curveDelta=0&reinvestBuy=false&reinvestSell=false&expiry=1722753360&lpFeeBp=0&buysideCreatorRoyaltyBp=10000&paymentMint=11111111111111111111111111111111&collectionSymbol=flashtrade&owner=EfdC1uyq5gWqHQYxSENscwNZKY8k2z8CXkpmPRB7mn2N
+
+
+// https://api-mainnet.magiceden.io/metrics/submit
+// POST
+// BODY {"metricsName":"sales.txlanding.latency","type":"gauge","val":686,"tags":["rpcEndpoint:https://solana.magiceden.dev","strategy:sendtx: send interval and confirm strategy","error:failed to send transaction: Transaction simulation failed: Error processing Instruction 2: custom program error: 0x1"]}
+
+
+// LIST
+
+// STEP 1// GET
+https://api-mainnet.magiceden.io/v2/instructions/batch?q=%255B%257B%2522type%2522%253A%2522m3_sell%2522%252C%2522ins%2522%253A%257B%2522seller%2522%253A%2522EfdC1uyq5gWqHQYxSENscwNZKY8k2z8CXkpmPRB7mn2N%2522%252C%2522assetId%2522%253A%2522DjFC31ZjgvNE1upqQSCDVFCaqo5XAU6EwtBKSrwhQxzF%2522%252C%2522price%2522%253A0.0054%252C%2522expiry%2522%253A-1%257D%257D%255D&prioFeeMicroLamports=3&maxPrioFeeLamports=300000
+
+
+// STEP 2 - POST
+https://api-mainnet.magiceden.io/v2/tx/3RYh8H9P6GipDTUs9ieePfU6p2cfREv3tQY8s2JJUbMCLMGwrTtY4EKyfC9xwURVtKQ4bHv6rSZGu4nm9DEqhEDT
+
+// BODY
+
+
+// https://api-mainnet.magiceden.io/rpc/getNFTsByOwner/EfdC1uyq5gWqHQYxSENscwNZKY8k2z8CXkpmPRB7mn2N?status=both&offset=0&limit=100&direction=1&field=2&token22StandardFilter=1&mplCoreStandardFilter=1&agg=3&compressionMode=both&itemVisibilityMode=VISIBLE_ONLY
