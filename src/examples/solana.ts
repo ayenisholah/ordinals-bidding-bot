b41c6f08bc1b0549e28fac47f0ce5f4850d99dfc
Shola Ayeni <ayenisholah@yahoo.com>
2024-07-05 12:18:50 +0100

    update solana docs

--- src/examples/solana.ts
+++ src/examples/solana.ts
@@ -1,21 +1,66 @@
-// https://api-mainnet.magiceden.io/v2/instructions/mmm/create-pool?spotPrice=0.00525&solDeposit=0.00525&curveType=exp&curveDelta=0&reinvestBuy=false&reinvestSell=false&expiry=1722753360&lpFeeBp=0&buysideCreatorRoyaltyBp=10000&paymentMint=11111111111111111111111111111111&collectionSymbol=flashtrade&owner=EfdC1uyq5gWqHQYxSENscwNZKY8k2z8CXkpmPRB7mn2N
+import axios from "axios";
+import { config } from "dotenv";
+import web3 from '@solana/web3.js'
 
+config()
 
-// https://api-mainnet.magiceden.i/metrics/submit
-// POST
-// BODY {"metricsName":"sales.txlanding.latency","type":"gauge","val":686,"tags":["rpcEndpoint:https://solana.magiceden.dev","strategy:sendtx: send interval and confirm strategy","error:failed to send transaction: Transaction simulation failed: Error processing Instruction 2: custom program error: 0x1"]}
+const API_KEY = process.env.API_KEY as string;
+const headers = {
+  'Content-Type': 'application/json',
+  'X-NFT-API-Key': API_KEY,
+}
 
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY as string;
+const rpcURL = `https://solana-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}`
 
-// LIST
+const connection = new web3.Connection(
+  rpcURL,
+  'confirmed',
+);
 
-// STEP 1// GET
-https://api-mainnet.magiceden.io/v2/instructions/batch?q=%255B%257B%2522type%2522%253A%2522m3_sell%2522%252C%2522ins%2522%253A%257B%2522seller%2522%253A%2522EfdC1uyq5gWqHQYxSENscwNZKY8k2z8CXkpmPRB7mn2N%2522%252C%2522assetId%2522%253A%2522DjFC31ZjgvNE1upqQSCDVFCaqo5XAU6EwtBKSrwhQxzF%2522%252C%2522price%2522%253A0.0054%252C%2522expiry%2522%253A-1%257D%257D%255D&prioFeeMicroLamports=3&maxPrioFeeLamports=300000
+async function main() {
+  try {
 
+    executeList()
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-// STEP 2 - POST
-https://api-mainnet.magiceden.io/v2/tx/3RYh8H9P6GipDTUs9ieePfU6p2cfREv3tQY8s2JJUbMCLMGwrTtY4EKyfC9xwURVtKQ4bHv6rSZGu4nm9DEqhEDT
+async function executeList() {
+  try {
 
-// BODY
+    const url = 'https://nfttools.pro/magiceden/v2/instructions/sell';
+    const params = {
+      seller: 'EfdC1uyq5gWqHQYxSENscwNZKY8k2z8CXkpmPRB7mn2N',
+      auctionHouseAddress: 'E8cU1WiRWjanGxmn96ewBgk9vPTcL6AEZ1t6F6fkgUWe',
+      tokenMint: '7kpQSvaSW5r4usqUHeqXCHK79zj4amadLdv1mpFpDiWR',
+      tokenAccount: 'HLfiWCHHma8BJDHPLt1q65v2cA4UXQKEswPCT1rEbQi4',
+      price: 1
+    };
+    const { data } = await axios.get(url, { params, headers })
+    JSON.stringify(data)
+    return data.tx.data
+  } catch (error) {
+    console.log(error);
+  }
+}
 
+async function executeBid() {
+  try {
+    const params = {
+      buyer: 'EfdC1uyq5gWqHQYxSENscwNZKY8k2z8CXkpmPRB7mn2N',
+      auctionHouseAddress: 'E8cU1WiRWjanGxmn96ewBgk9vPTcL6AEZ1t6F6fkgUWe',
+      tokenMint: '7kpQSvaSW5r4usqUHeqXCHK79zj4amadLdv1mpFpDiWR',
+      price: 1
+    };
+    const url = 'https://nfttools.pro/magiceden/v2/instructions/buy'
+    const { data } = await axios.get(url, { params, headers })
+    console.log(JSON.stringify(data));
+    return data
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-// https://api-mainnet.magiceden.io/rpc/getNFTsByOwner/EfdC1uyq5gWqHQYxSENscwNZKY8k2z8CXkpmPRB7mn2N?status=both&offset=0&limit=100&direction=1&field=2&token22StandardFilter=1&mplCoreStandardFilter=1&agg=3&compressionMode=both&itemVisibilityMode=VISIBLE_ONLY
+main()
